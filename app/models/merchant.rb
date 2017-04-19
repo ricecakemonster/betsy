5f8f4d3303c275bad593cc724106916bae3fb4a0
Ting Wong <hellotingwong@gmail.com>
2017-04-19 16:09:02 -0700

    added validation for merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,6 @@ class Merchant < ApplicationRecord
   has_many :products
   has_many :orders, through: :merchants_orders
   validates :merchant_name, presence: true
-  validates :merchant_email, presence: true
+  validates :merchant_email, presence: true, uniqueness: true
   validates :username, presence: true
 end

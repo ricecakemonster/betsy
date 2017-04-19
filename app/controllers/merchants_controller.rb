a75732808323c4ca9970d44f5fdac343073eda3f
Ting Wong <hellotingwong@gmail.com>
2017-04-19 14:32:18 -0700

    creating new merchant creation

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -10,6 +10,14 @@ class MerchantsController < ApplicationController
   end
 
   def create
+    @merchant = Merchant.create(merchant_params)
+    if @merchant.id != nil
+      flash[:success] = "Success, your merchant account is created!"
+      redirect_to root_path
+    else
+      flash[:failure] = "Sorry, your account was created unsuccessfully. Please try again."
+      render :new, status: :bad_request
+    end
   end
 
   def edit
@@ -19,5 +27,10 @@ class MerchantsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def merchant_params
+    return params.require(:merchant).permit(:merchant_name, :merchant_email, :username)
   end
 end

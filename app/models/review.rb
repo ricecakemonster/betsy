f33df3cceb52373b49e8bc99486590a1122332a2
Andrea Valliere <amvalliere@gmail.com>
2017-04-19 16:28:25 -0700

    Added model validations for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5], message: "%{value} rating value must be between 1 and 5"}
 end

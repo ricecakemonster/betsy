5318696f2122de572a0138cff4459833b2b13c43
Andrea Valliere <amvalliere@gmail.com>
2017-04-18 19:44:35 -0700

    Created controllers for merchants, orders, products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,46 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/show'
+
+  get 'products/new'
+
+  get 'products/create'
+
+  get 'products/edit'
+
+  get 'products/update'
+
+  get 'products/destroy'
+
+  get 'orders/index'
+
+  get 'orders/show'
+
+  get 'orders/new'
+
+  get 'orders/create'
+
+  get 'orders/edit'
+
+  get 'orders/update'
+
+  get 'orders/destroy'
+
+  get 'merchants/index'
+
+  get 'merchants/show'
+
+  get 'merchants/new'
+
+  get 'merchants/create'
+
+  get 'merchants/edit'
+
+  get 'merchants/update'
+
+  get 'merchants/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "products#index"
 

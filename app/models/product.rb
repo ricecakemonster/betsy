16b965a44c0d2956004b6e4391c5c7f898c7fcf2
Andrea Valliere <amvalliere@gmail.com>
2017-04-19 16:08:03 -0700

    Added validations for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   belongs_to :merchant
   has_many :orders, through: :orders_products
   has_many :reviews
+  validates :product_name, presence: true
+  validates :price, presence: true
+  validates :merchant_id, presence: true
+  validates :stock, presence: true
 end

fcc5437d051d512ec86d5cbb1b16fb2f49487f73
Andrea Valliere <amvalliere@gmail.com>
2017-04-18 19:33:57 -0700

    Added relations for models

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,4 @@
 class Merchant < ApplicationRecord
   has_many :products
-  has_many :orders, through: :
+  has_many :orders, through: :merchants_orders
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  has_many :merchants, through: :merchants_orders
+  has_many :products, through: :orders_products
 end

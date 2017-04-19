750b27e6adfe34d3e70e3d9500df06d54ab21e4b
Andrea Valliere <amvalliere@gmail.com>
2017-04-19 16:14:43 -0700

    Added model validations for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
   has_many :merchants, through: :merchants_orders
   has_many :products, through: :orders_products
+  validates :status, presence: true, inclusion: { in: %w(pending paid complete cancelled), message: "%{value} is not a valid status."}
+  validates :cc_num, presence: true
+  validates :cc_name, presence: true
+  validates :order_email, presence: true
+  validates :mailing_address, presence: true
+  validates :cc_expiry, presence: true
 end

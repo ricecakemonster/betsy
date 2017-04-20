0bbeca71aa9456fe24f14f62b927c16a29e4432e
Andrea Valliere <amvalliere@gmail.com>
2017-04-19 17:05:40 -0700

    Added model validations tests for products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,9 +1,31 @@
 require "test_helper"
 
 describe Product do
-  let(:product) { Product.new }
+  describe "validations" do
+    it "creates product with product_name" do
+      product = Product.create(product_name: "Isabelle's Raincoat")
+      product.errors.messages.wont_include :product_name
+    end
 
-  it "must be valid" do
-    value(product).must_be :valid?
+    it "creates product with price" do
+      product = Product.create(price: 20.95)
+      product.errors.messages.wont_include :price
+    end
+
+    it "is invalid without a product name" do
+      product = Product.new
+      result = product.valid?
+      result.must_equal false
+
+      product.errors.messages.must_include :product_name
+    end
+
+    it "is invalid without a price" do
+      product = Product.new
+      result = product.valid?
+      result.must_equal false
+
+      product.errors.messages.must_include :price
+    end
   end
 end

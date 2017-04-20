84486be06ed9f3873f6905a2070aabc5e46b5207
Ting Wong <hellotingwong@gmail.com>
2017-04-20 15:16:59 -0700

    changes master

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,9 +1,18 @@
 require "test_helper"
 
 describe Merchant do
-  let(:merchant) { Merchant.new }
+  describe "validations" do
+    it "can create a merchant with a merchant_name" do
+      merchant = Merchant.create(merchant_name: "Isabelle")
+      merchant.errors.messages.wont_include :merchant_name
+    end
 
-  it "must be valid" do
-    value(merchant).must_be :valid?
+    it "is invalid without a merchant_name" do
+      merchant = Merchant.new
+      result = merchant.valid?
+      result.must_equal false
+
+      merchant.errors.messages.must_include :merchant_name
+    end
   end
 end

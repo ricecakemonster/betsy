222808df1ffba261796e215b12bfb1d3fce74816
Ting Wong <hellotingwong@gmail.com>
2017-04-23 16:07:25 -0700

    added merchant model validation testing, also revised merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,18 @@ class Merchant < ApplicationRecord
   has_many :products
   has_many :orders, through: :products
   validates :merchant_name, presence: true
-  validates :merchant_email, presence: true
+  validates :merchant_email, presence: true, uniqueness: true
   validates :username, presence: true
+  validates :oauth_uid, presence: true
+  validates :oauth_provider, presence: true
+
+  def self.from_github(auth_hash)
+    merchant = Merchant.new
+    merchant.username = auth_hash["info"]["nickname"]
+    merchant.merchant_name = auth_hash["info"]["name"]
+    merchant.merchant_email = auth_hash["info"]["email"]
+    merchant.oauth_uid = auth_hash["info"]["uid"]
+    merchant.oauth_provider = "github"
+    return merchant
+  end
 end

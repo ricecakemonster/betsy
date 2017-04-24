1e3eb39e97add37085ce74f2afb4a2c2eeea9892
Ting Wong <hellotingwong@gmail.com>
2017-04-24 12:59:50 -0700

    updated merchant view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,17 +45,17 @@ class ProductsController < ApplicationController
   def destroy
   end
 
-  def review_new
-    @review = Review.new(params[:id])
-  end
+  # def review_new
+  #   @review = Review.new(params[:id])
+  # end
 
   def review_create
     product = Product.find(params[:product_id])
     review_info = {
       product_id: product.id,
       rating: review_params[:rating]
-      nickname: review_params[:nickname]
-      review_description: review_params[:review_description]
+      # nickname: review_params[:nickname]
+      # review_description: review_params[:review_description]
     }
     @review = product.reviews.build(review_info)
     if @review.save
@@ -68,5 +68,9 @@ class ProductsController < ApplicationController
 private
   def product_params
     params.require(:product).permit(:product_name, :price, :merchant_id, :photo_url, :stock, :product_description)
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :nickname, :review_description)
   end
 end

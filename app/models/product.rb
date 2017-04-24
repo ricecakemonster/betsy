b3cc79bd094451ce38d49cc2b507a09248f31ac7
Andrea Valliere <amvalliere@gmail.com>
2017-04-24 10:36:03 -0700

    Fixing git history

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@ class Product < ApplicationRecord
   has_many :orders, through: :orderproducts
   has_many :orderproducts
   has_many :reviews
+  validates :product_name, presence: true
+  validates :price, presence: true
+  validates :merchant_id, presence: true
+  validates :stock, presence: true
 end

69ff19a6d24239cb8a38c3fee8d90728996bf16a
Ting Wong <hellotingwong@gmail.com>
2017-04-24 11:46:34 -0700

    working copy of reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,6 +45,26 @@ class ProductsController < ApplicationController
   def destroy
   end
 
+  def review_new
+    @review = Review.new(params[:id])
+  end
+
+  def review_create
+    product = Product.find(params[:product_id])
+    review_info = {
+      product_id: product.id,
+      rating: review_params[:rating]
+      nickname: review_params[:nickname]
+      review_description: review_params[:review_description]
+    }
+    @review = product.reviews.build(review_info)
+    if @review.save
+      redirect_to product_path(product.id)
+    else
+      render :new
+    end
+  end
+
 private
   def product_params
     params.require(:product).permit(:product_name, :price, :merchant_id, :photo_url, :stock, :product_description)

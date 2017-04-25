373ab2f123b15aa593d1518f773f98afd482adfe
Ting Wong <hellotingwong@gmail.com>
2017-04-24 20:55:12 -0700

    merchants cannot review their own products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   has_many :orders, through: :orderproducts
   has_many :orderproducts
   has_many :reviews
-  
+
   def avg_rating
     avg = reviews.select('avg(reviews.rating) as avg_rating').take&.avg_rating
     unless avg.nil?
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,12 +1,3 @@
 class Review < ApplicationRecord
-  belongs_to :product
-
-  # def avg_rating
-  #   avg = reviews.select('avg(reviews.rating) as avg_rating').take&.avg_rating
-  #   unless avg.nil?
-  #     return avg.round(2)
-  #   else
-  #     return nil
-  #   end
-  # end
+  belongs_to :product  
 end

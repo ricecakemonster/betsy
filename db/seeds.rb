bb51694f4df72867afb032a7e60bffa9b6bcc6a6
EricaJCase <ericajcase@gmail.com>
2017-04-26 14:37:53 -0700

    unsuccessful database problem solving

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,7 @@ media_file = Rails.root.join('db', 'seeds.csv')
   Merchant.create!(merchant_name: Faker::Name.first_name, merchant_email: merchant_email, username: username, oauth_uid: oauth_uid, oauth_provider: oauth_provider)
 end
 
-CSV.foreach(media_file, headers: true, header_converters: :symbol, converters: :all, skip_blanks: true) do |row|
+CSV.foreach(media_file, headers: true, header_converters: :symbol, converters: :all, skip_blanks: false) do |row|
   data = Hash[row.headers.zip(row.fields)]
-  if data[:image_url].nil?
-    data.delete(:image_url)
-  puts data
   Product.create!(data)
 end

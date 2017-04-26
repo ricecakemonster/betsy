feed4d216b66f24c603d64a9f4c03108b9bc870e
Andrea Valliere <amvalliere@gmail.com>
2017-04-25 22:36:11 -0700

    fixed more merge conflicts from merge with master-1

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,12 +3,11 @@ class Product < ApplicationRecord
   has_many :orders, through: :orderproducts
   has_many :orderproducts
   has_many :reviews
-<<<<<<< HEAD
   validates :product_name, presence: true
   validates :price, presence: true
   validates :merchant_id, presence: true
   validates :stock, presence: true
-=======
+
 
   def avg_rating
     avg = reviews.select('avg(reviews.rating) as avg_rating').take&.avg_rating
@@ -18,5 +17,4 @@ class Product < ApplicationRecord
       return nil
     end
   end
->>>>>>> master-1
 end

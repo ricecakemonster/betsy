79671e6bca16ba76445f61115a74fc1bc5039f60
Ting Wong <hellotingwong@gmail.com>
2017-04-26 22:44:40 -0700

    removed old seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,6 @@ require 'csv'
 product_file = Rails.root.join('db', 'product_seeds.csv')
 merchant_file = Rails.root.join('db', 'merchant_seeds.csv')
 
-# 50.times do
-#   merchant_email = Faker::Name.last_name + Faker::Number.number(2) + "@betsy.com"
-#   username = Faker::Color.color_name + "_" + Faker::Hipster.word + Faker::Number.number(2)
-#   oauth_uid = Faker::Number.number(5)
-#   oauth_provider = "github"
-#   Merchant.create!(merchant_name: Faker::Name.first_name, merchant_email: merchant_email, username: username, oauth_uid: oauth_uid, oauth_provider: oauth_provider)
-# end
-
 CSV.foreach(merchant_file, headers: true, header_converters: :symbol, converters: :all, skip_blanks: false) do |row|
   data = Hash[row.headers.zip(row.fields)]
   Merchant.create!(data)

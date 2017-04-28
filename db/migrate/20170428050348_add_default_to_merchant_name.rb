fe478b6b65452150434583c62895f4973cf09613
Ting Wong <hellotingwong@gmail.com>
2017-04-28 09:21:26 -0700

    merged ting's css

--- db/migrate/20170428050348_add_default_to_merchant_name.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultToMerchantName < ActiveRecord::Migration[5.0]
-  def change
-    change_column_default :merchants, :merchant_name, :username
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428050348) do
+ActiveRecord::Schema.define(version: 20170428161114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,9 +79,9 @@ ActiveRecord::Schema.define(version: 20170428050348) do
     t.integer  "product_id"
     t.integer  "rating"
     t.string   "review_description"
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
-    t.string   "nickname",           default: "Anonymous Customer"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "nickname"
     t.index ["product_id"], name: "index_reviews_on_product_id", using: :btree
   end
 

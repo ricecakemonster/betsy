0274a0e5e3239612d0ecae7aff85414b8de8f5f1
EricaJCase <ericajcase@gmail.com>
2017-04-27 18:41:36 -0700

    fixed logout error created by last merge

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,6 @@ Rails.application.routes.draw do
   resources :reviews
 
   get "/auth/:provider/callback", to: "sessions#login", as: 'auth_callback'
-  post '/login', to: 'sessions#login'
-  post '/logout', to: 'sessions#logout', as: 'logout'
+  post '/login', to: 'merchants#login'
+  post '/logout', to: 'merchants#logout', as: 'logout'
 end
